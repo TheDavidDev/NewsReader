db2b9e939759ac5099191c4e5120478c6b3bab90
𝔻ℕ𝕠𝕣𝕕𝕚𝕔𝕍𝕫𝕝𝕒𝕟 <sthlm412@gmail.com>
2020-09-21 17:28:27 +0200

    upload to github

--- src/App.js
+++ src/App.js
@@ -1,71 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import { Typography } from '@material-ui/core';
-import wordsToNumbers from 'words-to-numbers';
-import alanBtn from '@alan-ai/alan-sdk-web';
+import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-import logo from './images/logo.png';
-import { NewsCards, Modal } from './components';
-import useStyle from './styles.js';
+import logo from "./images/logo.png";
+// import { NewsCards, Modal } from './components';
+import Modal from "./components/Modal/Modal.js";
+import NewsCards from "./components/NewsCards/NewsCards.js";
+import useStyle from "./style.js";
 
 const App = () => {
-    const [activeArticle, setActiveArticle] = useState(0);
-    const [newsArticles, setNewsArticles] = useState([]);
-    const [isOpen, setIsOpen] = useState(false);
-   
-    const classes = useStyle();
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-      alanBtn({
-          key: '55d10b0d7184a8f4ccdbf905d86ddd202e956eca572e1d8b807a3e2338fdd0dc/stage',
-          onCommand: ({ command, articles, number }) => {
-            if (command === 'newHeadlines') {
-               setNewsArticles(articles);
-               setActiveArticle(-1);
-            }  else if (command === 'instructions') {
-               setIsOpen(true);
-            }  else if(command === 'highlight') {
-               setActiveArticle((preActiveArticle) => preActiveArticle + 1);
-            }  else if (command === 'open') {
-               const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true}) : number;
-               const article = articles[parsedNumber - 1];
-               
-               if (parsedNumber > 20) {
-                 alanBtn().playText('Please try that again...');
-               } else if (article) {
-                 window.open(article.url, '_blank');
-                 alanBtn().playText('Opening...');
-               } else {
-                 alanBtn().playText('Please try that again...');  
-               }
-            }
-           },
-        });
-     }, []);
+  const classes = useStyle();
 
-    return (
-        <div>
-            <div className={classes.logoContainer}>
-             {newsArticles.length ? (
-               <div className={classes.infoContainer}>
-                 <div className={classes.card}><Typography variant="h5" component="h2">Try saying:<br /><br />Open article number [4]</Typography></div>     
-                 <div className={classes.card}><Typography variant="h5" component="h2">Try saying:<br /><br />Go back</Typography></div>
-               </div> 
-             ) : null}   
-             <img src="https://alan.app/voice/images/previews.jpg" className={classes.alanLogo} alt="alan logo"/>
+  useEffect(() => {
+    alanBtn({
+      key:
+        "55d10b0d7184a8f4ccdbf905d86ddd202e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((preActiveArticle) => preActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying:
+                <br />
+                <br />
+                Open article number [4]
+              </Typography>
             </div>
-            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
-            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
-            {!newsArticles.length ? ( 
-              <div className={classes.footer}>
-                <Typography variant="body1" component="h2">
-                    Created by 
-                    <a className={classes.link} href="https://linkedin.com/in/TheDavidDev"> The David Deveveloper</a>
-                </Typography>
-                <img className={classes.image} src={logo} height="50px" alt="David The Developer logo"/>
-                </div>
-            ) : null}
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying:
+                <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="alan logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="https://linkedin.com/in/TheDavidDev"
+            >
+              {" "}
+              The David Deveveloper
+            </a>
+          </Typography>
+          <img
+            className={classes.image}
+            src={logo}
+            height="50px"
+            alt="David The Developer logo"
+          />
         </div>
-    );
+      ) : null}
+    </div>
+  );
 };
 
 export default App;
