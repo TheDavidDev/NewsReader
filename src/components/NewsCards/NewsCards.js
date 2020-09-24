c2753f6984ebe4f01f6c1d587659809920057af1
David The Developer <david.lopez@hyperisland.se>
2020-09-24 18:39:31 +0200

    Update NewsCards.js
    add information to the cards

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,13 +5,13 @@ import NewsCard from "./NewsCard/NewsCard";
 import useStyle from "./styles.js";
 
 const infoCards = [
-  { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
+  { color: "#00838f", title: "Latest News", text: "Give me the latest news, or just say thanks Alan." },
   {
     color: "#1565c0",
     title: "News by Categories",
     info:
       "Business, Entertainment, General, Health, Science, Sports, Technology",
-    text: "Give me the latest Technology news",
+    text: "Give me the latest Sports news",
   },
   {
     color: "#4527a0",
